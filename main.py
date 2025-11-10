e639d42224b3f85f79b6e4e47a8a7da2d5e05ce0
DSoter <davidgn.04@gmail.com>
2025-11-10 13:20:38 +0100

    Main Act1

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from typing import Dict, List, Tuple
 from fastapi import FastAPI
 from pydantic import BaseModel
+import bisect
 
 app = FastAPI()
 
-class Producto(BaseModel):
+class Product(BaseModel):
     id: int
-    nombre: str
-    categorias: List[Tuple[str,float]]
+    name: str
+    categories: List[Tuple[str,float]] #Lista que contiene las categorias a las cuales pertenece el producto y su ponderación
 
 class Cliente(BaseModel):
     id : int
     user : str
-    categoriasCli : Dict[str,int]
-    comprados : Dict[int, Producto]
-    preferencias : List[str]
-    restricciones : List[str]
+    categories : Dict[str,int]
+    boughtProducts : Dict[int, Product]
+    preferences : List[str]
+    restrictions : List[str]
+    allProducts : List[Product]
+
+    def recomendar (self, num:int):
+        recomended = []
+        for product in self.allProducts
+
+
+
+
